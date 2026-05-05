76bbe906dc8c1a5728297de48e4b097ac22f4631
Andres Lot <lotcamandres@gmail.com>
2026-05-05 23:46:30 +0200

    chore(stickers): Add const with template values

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+#include "sticker.h"
 
-int main(int argc, *char argv[]) {
-        return 0;
+int main(int argc, char *argv[]) {
+    Sticker *sticker = sticker_find(CATALOG, 980, "MEX01");
+    return 0;
 }

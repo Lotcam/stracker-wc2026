2f880d0972f6c2a74363b93ee2608574baf1b755
Andres Lot <lotcamandres@gmail.com>
2026-05-11 12:55:36 +0200

    chore(storage): Create storage when missing

--- main.c
+++ main.c
@@ -8,6 +8,10 @@
 int main(int argc, char *argv[]) {
     int count = MAX_STICKERS;
     Sticker *stickers = load_db("storage.dat", &count);
+    if (!stickers) {
+        save_db("storage.dat", CATALOG, count);
+        stickers = load_db("storage.dat", &count);
+    }
     if (argc < 2) {
         return 0;
     }
@@ -18,6 +22,11 @@ int main(int argc, char *argv[]) {
         else if (strcmp(argv[1], "add") == 0) {
             if (argc < 3) return 0;
             sticker_add(stickers, &count, argv[2]);
+            save_db("storage.dat", stickers, count);
+        }
+        else if (strcmp(argv[1], "remove") == 0) {
+            if (argc < 3) return 0;
+            sticker_remove(stickers, &count, argv[2]);
             save_db("storage.dat", stickers, count);
         }
     }
